316c5badd5ec4ade2a5bda4fb33cd0b43fc71070
Mert Toprak <mert.tprk@gmail.com>
2024-02-27 15:50:04 +0100

    added cors options

--- server/app.js
+++ server/app.js
@@ -9,7 +9,12 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+var corsOptions = {
+	origin: 'https://ecemertwedding.netlify.app',
+	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const rsvpRoutes = require('./routes/rsvpRoutes');
