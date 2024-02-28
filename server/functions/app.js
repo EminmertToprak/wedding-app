be59aac116eb5f94a599c397d930917659c79e6d
Mert Toprak <mert.tprk@gmail.com>
2024-02-28 18:49:31 +0100

    netlify serverless function test

--- server/functions/app.js
+++ server/functions/app.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const connectDB = require('../db');
-
 const app = express();
 
-app.use(express.json());
-
-app.use((req, res, next) => {
-	console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-	next();
-});
-
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
 	res.send('Hello from Express.js running on Netlify!');
 });
 
-const registerRsvp = require('../routes/rsvpRoutes_2');
-
-registerRsvp(app);
-
 module.exports.handler = async (event, context) => {
-	connectDB();
 	const { path, httpMethod, queryStringParameters, body } = event;
 	const req = {
 		method: httpMethod,
@@ -42,7 +28,3 @@ module.exports.handler = async (event, context) => {
 		body: res._body,
 	};
 };
-
-// // Start the server
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
