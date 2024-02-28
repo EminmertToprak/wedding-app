f924a6472c0f973f099a62a6652984527cb1dc3b
Mert Toprak <mert.tprk@gmail.com>
2024-02-28 18:22:36 +0100

    semih test

--- server/app.js
+++ server/app.js
@@ -10,25 +10,30 @@ connectDB();
 // Middleware
 const cors = require('cors');
 
-const corsOptions = {
-	origin: 'https://ecemertwedding.netlify.app', // Replace with your frontend URL
-	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+// 	origin: 'https://ecemertwedding.netlify.app', // Replace with your frontend URL
+// 	optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 
-// // Custom middleware to log requests (Server Logging)
-// app.use((req, res, next) => {
-// 	console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
-// 	next();
-// });
+// Custom middleware to log requests (Server Logging)
+app.use((req, res, next) => {
+	console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+	next();
+});
 
-const rsvpRoutes = require('./routes/rsvpRoutes');
+const registerRsvp = require('./routes/rsvpRoutes_2');
 
 // Use RSVP routes
-app.use('/rsvp', rsvpRoutes);
+// app.use('/rsvp', rsvpRoutes);
+registerRsvp(app);
+
+app.get('/test', async (req, res) => {
+	res.status(200).json({ message: 'test' });
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
