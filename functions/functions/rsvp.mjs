b71aa8d2d2c6de329b27b27b4ba83f40be059c87
Mert Toprak <mert.tprk@gmail.com>
2024-03-04 19:04:36 +0100

    added cors headers to options condition

--- functions/functions/rsvp.mjs
+++ functions/functions/rsvp.mjs
@@ -2,11 +2,20 @@ const db = require('../service/db.mjs').default;
 const RSVP = require('../models/rsvp.model');
 
 export default async (req, context) => {
-	if (req.method !== 'POST') {
+	if (req.method === 'OPTIONS') {
+		return new Response(null, {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Access-Control-Allow-Methods': 'POST',
+				'Access-Control-Allow-Headers': 'Content-Type',
+			},
+		});
+	} else if (req.method !== 'POST') {
 		return new Response('BAD REQUEST', {
 			status: 400,
 		});
 	}
+
 	try {
 		const chunks = [];
 		for await (const chunk of req.body) {
