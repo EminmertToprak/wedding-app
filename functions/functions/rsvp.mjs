d8b89bc5073e2739d58181705cdae4565c80133c
Mert Toprak <mert.tprk@gmail.com>
2024-07-08 20:33:58 +0200

    Backend Test

--- functions/functions/rsvp.mjs
+++ functions/functions/rsvp.mjs
@@ -1,50 +1,69 @@
-const db = require('../service/db.mjs').default;
+const db = require('../service/db.mjs');
 const RSVP = require('../models/rsvp.model');
 
-export default async (req, context) => {
-	const headers = {
+const sharedResponse = {
+	headers: {
 		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': 'POST',
+		'Access-Control-Allow-Methods': 'POST, GET',
 		'Access-Control-Allow-Headers': 'Content-Type',
-	};
+	},
+};
 
-	// Apply CORS headers to all responses
-	const responseInit = {
+export default async (req, context) => {
+	if (req.method === 'OPTIONS') return HandleOptions(req, context);
+	if (req.method === 'POST') return HandlePost(req, context);
+	if (req.method === 'GET') return HandleGet(req, context);
+
+	return new Response('BAD REQUEST', {
+		status: 400,
 		headers: headers,
-	};
+	});
+};
 
-	// Check if it's an OPTIONS request and handle it separately
-	if (req.method === 'OPTIONS') {
-		return new Response(null, responseInit);
-	}
+//[OPTIONS] /rsvp
+async function HandleOptions(req, context) {
+	return new Response(null, sharedResponse);
+}
 
-	// Handle non-POST requests
-	if (req.method !== 'POST') {
-		return new Response('BAD REQUEST', {
-			status: 400,
-			headers: headers,
-		});
-	}
-
+//[POST] /rsvp
+async function HandlePost(req, context) {
 	try {
-		const chunks = [];
-		for await (const chunk of req.body) {
-			chunks.push(chunk);
-		}
-		const bodyBuffer = Buffer.concat(chunks);
-		const requestBody = JSON.parse(bodyBuffer.toString());
+		var body = ParseRequestBody(req);
 		const newRSVP = new RSVP(requestBody);
-		db();
+		db.connectDB();
 		await newRSVP.save();
-		return new Response(`SUCCESS!`, responseInit);
+		return new Response(`SUCCESS!`, sharedResponse);
 	} catch (error) {
 		console.error('Error submitting RSVP:', error);
 		return new Response(`Error: ${error}`, {
 			status: 400,
-			headers: headers,
+			headers: sharedResponse.headers,
 		});
 	}
-};
+}
+
+async function HandleGet(req, context) {
+	try {
+		db.connectDB();
+		const rsvps = await RSVP.find();
+		console.log(rsvps);
+		return new Response(JSON.stringify(rsvps), sharedResponse);
+	} catch (error) {
+		console.error('Error getting RSVP:', error);
+		return new Response(`Error: ${error}`, {
+			status: 400,
+			headers: sharedResponse.headers,
+		});
+	}
+}
+async function ParseRequestBody(req) {
+	const chunks = [];
+	for await (const chunk of req.body) {
+		chunks.push(chunk);
+	}
+	const bodyBuffer = Buffer.concat(chunks);
+	return JSON.parse(bodyBuffer.toString());
+}
 
 export const config = {
 	path: '/rsvp',
