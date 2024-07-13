8aa164cfdf73e6b35de200f80618a3bf7ebcc05d
Mert Toprak <mert.tprk@gmail.com>
2024-07-14 00:07:15 +0200

    re-added Admin page

---
+++ client/src/pages/Admin.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../css/admin.css';
+import AdminTable from '../components/AdminTable.tsx';
+
+const Admin: React.FC = () => {
+	return (
+		<div className="admin-page">
+			<h1 className="text-center">Admin Page - RSVP Elements</h1>
+			<AdminTable />;
+		</div>
+	);
+};
+export default Admin;
