3947eba6488b671afa13094998ebfb03e040d8cc
Mert Toprak <mert.tprk@gmail.com>
2024-07-14 00:22:58 +0200

    added loader back

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,16 @@
+import '../css/loader.css';
+import React from 'react';
+
+const Loader: React.FC = () => {
+	return (
+		<div className="sound-wave">
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	);
+};
+
+export default Loader;
